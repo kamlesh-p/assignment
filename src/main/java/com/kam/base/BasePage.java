c3b8c297d40a1d9a845c45dfc1366fe7be7b58f6
Kamlesh <kamleshpes@gmail.com>
2021-09-22 23:07:50 +0530

    Initial setup

---
+++ src/main/java/com/kam/base/BasePage.java
@@ -0,0 +1,16 @@
+package com.kam.base;
+
+public class BasePage {
+	
+	WebDriver driver;
+	
+	public BasePage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	
+	private void clickElement() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/main/java/com/kam/pages/HomePage.java
@@ -0,0 +1,5 @@
+package com.kam.pages;
+
+public class HomePage {
+
+}
